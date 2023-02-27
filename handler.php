88d9cf0a328d53b4c98cd4cf1685fbd4b4e7f039
AidenVu1 <aiden.vu@stu.ocsb.ca>
2023-02-27 10:35:05 -0500

    php

--- handler.php
+++ handler.php
@@ -2,6 +2,12 @@
 <html>
   <head>
     <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+    <link rel="susicon" sizes="180x180" href="./favicon/favicon.ico">
+    <link rel="icon" type="image/png" sizes="32x32" href="./favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./favicon/favicon-16x16.png">
+    <link rel="manifest" href="./images/site.webmanifest">
     <title>Page Title</title>
   </head>
 
